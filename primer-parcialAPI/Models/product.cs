e30f14edde2aa1406ec0461eeaa94ee2e66096d4
Brayan Zamora <a2024111327@estudiantes.upsa.edu.bo>
2025-09-29 20:50:51 -0400

    add-model-product

---
+++ primer-parcialAPI/Models/product.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace primer_parcialAPI.Models
+{
+    public sealed class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(120)]
+        public string Name { get; set; } = null!;
+
+        // nullable según el enunciado
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // nullable según el enunciado
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
