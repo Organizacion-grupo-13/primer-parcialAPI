0cc23d75709668c7b14581b0417b2ff79a132a5f
Jose Mario Nuñez Justiniano <a2024111939@estudiantes.upsa.edu.bo>
2025-09-29 20:57:48 -0400

    mod-Dbcontext

--- primer-parcialAPI/Data/AppDbContext.cs
+++ primer-parcialAPI/Data/AppDbContext.cs
@@ -1,15 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using primer_parcialAPI.Models;
 
 namespace primer_parcialAPI.Data
 {
     public sealed class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
-        // Aquí cada integrante añadirá su DbSet más adelante:
-        // public DbSet<Product> Products { get; set; }
-        // public DbSet<Event> Events { get; set; }
-        // public DbSet<SupportTicket> SupportTickets { get; set; }
+        public DbSet<Event> Events { get; set; }  // ← José
     }
 }
